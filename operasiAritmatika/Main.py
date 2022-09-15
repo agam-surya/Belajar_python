45586fcaac2bce0e6423a3e97c58c075c9592e3d
agam-surya <mirzahaqiqi04@gmail.com>
2022-09-15 10:16:46 +0700

    operasi aritmatika

---
+++ operasiAritmatika/Main.py
@@ -0,0 +1,39 @@
+a = 17
+b = 3
+
+# tambah
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+# kurang
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+# kali
+hasil = a * b
+print(a, "*", b, "=", hasil)
+
+# bagi
+hasil = a / b
+print(a, "/", b, "=", hasil)
+
+# eksponen/pangkat
+hasil = a ** b
+print(a, "**", b, "=", hasil)
+
+# modulus/sisa bagi
+hasil = a % b
+print(a, "%", b, "=", hasil)
+
+# floor division / kebalikan dari modulus (pembulatan dari pembagian float ke integer)
+hasil = a // b
+print(a, "//", b, "=", hasil)
+
+# URUTAN PRIORITAS ARITMATIKA
+'''
+1. ()
+2. **
+3. perkalian dll
+4. tambah dan kurang
+
+'''
